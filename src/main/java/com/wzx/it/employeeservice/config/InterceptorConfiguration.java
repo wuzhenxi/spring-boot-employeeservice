1827f84c4a1d85a43825cd528376a32c12213b54
wuzhenxi <wuzx@hustcad.com>
2019-11-19 20:22:41 +0800

    【type】:修改 【author】:wzx 【description】:webmvc配置修改

--- src/main/java/com/wzx/it/employeeservice/config/InterceptorConfiguration.java
+++ src/main/java/com/wzx/it/employeeservice/config/InterceptorConfiguration.java
@@ -1,14 +1,29 @@
 package com.wzx.it.employeeservice.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableAutoConfiguration
 public class InterceptorConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public CheckLoginInterceptor authenticationInterceptor() {
+        return new CheckLoginInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器
@@ -16,7 +31,6 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
                 // 配置拦截的路径
          .addPathPatterns("/**")
                 // 配置不拦截的路径
-                .excludePathPatterns("/**")
         .excludePathPatterns("/**.html","/error","/uploadFile");
         // 还可以在这里注册其它的拦截器
         //registry.addInterceptor(new OtherInterceptor()).addPathPatterns("/**");
@@ -30,8 +44,75 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
 //         expiredSessionStrategy(ajaxSessionInformationExpiredStrategy);
     }
 
-    @Bean
-    public CheckLoginInterceptor authenticationInterceptor() {
-        return new CheckLoginInterceptor();
+    /**
+     * 添加静态资源--过滤swagger-api (开源的在线API文档)
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //过滤swagger
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/swagger-resources/**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-resources/");
+
+        registry.addResourceHandler("/swagger/**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger*");
+
+        registry.addResourceHandler("/v2/api-docs/**")
+                .addResourceLocations("classpath:/META-INF/resources/v2/api-docs/");
+
+    }
+
+    /**
+     * 添加类型转换器和格式化器
+     * @param registry
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatterForFieldType(LocalDate.class, null);
+    }
+
+    /**
+     * 跨域支持
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600 * 24);
+    }
+
+    /**
+     * 配置消息转换器--这里我用的是alibaba 开源的 fastjson
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //1.需要定义一个convert转换消息的对象;
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        //2.添加fastJson的配置信息，比如：是否要格式化返回的json数据;
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteDateUseDateFormat);
+        //3处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        //4.在convert中添加配置信息.
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        //5.将convert添加到converters当中.
+        converters.add(fastJsonHttpMessageConverter);
     }
 }
