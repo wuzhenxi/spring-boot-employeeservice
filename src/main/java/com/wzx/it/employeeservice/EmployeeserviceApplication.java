1a0d3eedc6be1fba06bc584332982c8943b083d6
wuzhenxi <wuzx@hustcad.com>
2019-11-21 11:10:04 +0800

    【type】:修改 【author】:wzx 【description】:eureka注册中心引用spring-security

--- src/main/java/com/wzx/it/employeeservice/EmployeeserviceApplication.java
+++ src/main/java/com/wzx/it/employeeservice/EmployeeserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
